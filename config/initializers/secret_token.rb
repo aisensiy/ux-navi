6d405542bc9224142fc9187a9fe3bf52a19dbace
aisensiy <aisensiy@gmail.com>
2013-07-22 14:56:47 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UxNavi::Application.config.secret_token = '0334dfc2dfc3f30a4a645b566bd80d5c3e676c0f0c0d86e76df838c4decff9756951c18f1dc412e0fe2de950daeaea4a5bef8159589303db0dde26162d3e536a'
