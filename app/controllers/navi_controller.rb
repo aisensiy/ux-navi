19a24e94309d1fe8da099ff7821cec4ddf09cf13
aisensiy <aisensiy@gmail.com>
2013-08-05 10:51:04 +0800

    bug fix for pg

--- app/controllers/navi_controller.rb
+++ app/controllers/navi_controller.rb
@@ -4,7 +4,7 @@ class NaviController < ApplicationController
 
   def index
     @links = Link.includes(:tags, :categories)
-    @tags = Link.tag_counts.order('count desc').select { |elem| elem.count > 1 }
+    @tags = Link.tag_counts.order('count desc').select { |elem| elem.count.to_i > 1 }
     @categories = Category.pluck(:name).uniq
   end
 end
