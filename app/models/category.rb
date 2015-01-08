d2b7ce818a1ebe64e9943c9bb161975b92d592da
aisensiy <aisensiy@gmail.com>
2015-01-08 15:25:56 +0800

    add relationship between category and tag

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,5 @@ class Category < ActiveRecord::Base
 
   has_many :link_categories
   has_many :links, through: :link_categories
+  has_many :tags
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130819053726) do
+ActiveRecord::Schema.define(:version => 20150108072415) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -64,8 +64,9 @@ ActiveRecord::Schema.define(:version => 20130819053726) do
 
   create_table "tags", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.integer  "category_id"
   end
 
   create_table "users", :force => true do |t|
